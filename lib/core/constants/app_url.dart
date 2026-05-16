578f06f86aaa5b810ca603f5ee4e9ce3f16970cd
Anas Barakat <125308313+anasbarakat0@users.noreply.github.com>
2026-05-16 21:33:40 +0300

    Proxy API requests through Netlify

--- lib/core/constants/app_url.dart
+++ lib/core/constants/app_url.dart
@@ -1,1 +1,4 @@
-const String baseUrl = "http://api.lighthouse-hub.com:3010";
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+const String apiOrigin = "http://api.lighthouse-hub.com:3010";
+const String baseUrl = kIsWeb ? "/api-proxy" : apiOrigin;
